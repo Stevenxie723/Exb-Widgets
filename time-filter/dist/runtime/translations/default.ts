c4faac4331ef6b1296b60e9ee3131ac865d1fb07
Steven <stevenxie42@gmail.com>
2025-01-20 16:27:49 +0800

    Add time filter with multiple data sources

---
+++ time-filter/dist/runtime/translations/default.ts
@@ -0,0 +1,5 @@
+export default {
+  chooseStartDateLabel: '選擇起始日期',
+  chooseEndDateLabel: '選擇結束日期',
+  toLabel: '~'
+}
---
+++ time-filter/dist/runtime/translations/en.ts
@@ -0,0 +1,11 @@
+System.register([], function (_export) {
+  return {
+    execute: function () {
+      _export({
+        chooseStartDateLabel: 'Choose start date',
+        chooseEndDateLabel: 'Choose end date',
+        toLabel: '~'
+      })
+    }
+  }
+})
