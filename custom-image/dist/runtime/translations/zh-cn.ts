e4b3e3b3939e436dcabee656fe9028f60db26a39
Steven <stevenxie42@gmail.com>
2025-01-24 13:27:53 +0800

    Add custom-image (naive)

---
+++ custom-image/dist/runtime/translations/zh-cn.ts
@@ -0,0 +1,14 @@
+System.register([], function (_export) {
+  return {
+    execute: function () {
+      _export({
+        _widgetLabel: '示例',
+        widgetProperties: '组件属性',
+        widgetFunctions: '组件功能',
+        mapview: '地图视图',
+        widgetName: '名称:',
+        widgetProps: '属性:'
+      })
+    }
+  }
+})
